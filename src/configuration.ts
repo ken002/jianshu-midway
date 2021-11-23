0252d347b1908a03aa849856b71e730a49c98a74
jly <806784058@qq.com>
2021-11-23 15:14:18 +0800

    新增swagger

--- src/configuration.ts
+++ src/configuration.ts
@@ -1,6 +1,7 @@
 import { App, Configuration } from '@midwayjs/decorator';
 import { ILifeCycle } from '@midwayjs/core';
 import * as orm from '@midwayjs/orm';
+import * as swagger from '@midwayjs/swagger';
 import { Application } from 'egg';
 import { join } from 'path';
 
@@ -9,6 +10,7 @@ import { join } from 'path';
   conflictCheck: true,
   imports: [
     orm, // 加载 orm 组件
+    swagger,
   ],
 })
 export class ContainerLifeCycle implements ILifeCycle {
